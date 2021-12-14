6e48bf52e3e8a80b27e4e2bd6053745dfee2bb20
unknown <PS23FManrique@efcts.us>
2021-12-14 10:21:06 -0500

    Initial commit

---
+++ Assets/Scripts/ActiveProjectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveProjectile : MonoBehaviour
+{
+    public GameObject projectile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var clone = Instantiate(projectile, gameObject.transform.position, gameObject.transform.rotation);
+
+            //destroy after 2 secons to stop clutter.
+            Destroy(clone, 5.0f);
+        }        
+    }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpPower = 4;
+    Rigidbody rb;
+    CapsuleCollider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizontal = Input.GetAxis("Horizontal") * speed;
+        float Vertical = Input.GetAxis("Vertical") * speed;
+
+        Horizontal *= Time.deltaTime;
+        Vertical *= Time.deltaTime;
+
+        transform.Translate(Horizontal, 0, Vertical);
+
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+        {
+            //add upward force to the rigid body when we press jump.
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    private bool isGrounded()
+    {
+        //test that we are grounded by drawing an invisible like
+        //if this hits a solid object then we are grounded
+
+        return Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 0.1f);
+    }
+}
